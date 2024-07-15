7a474898147d17418339cc2308529327bf6903c3
Alexis Gutierrez <jospers557@gmail.com>
2024-07-14 22:55:04 -0500

    feat: navigation routes

--- Apps/Alumno/views.py
+++ Apps/Alumno/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Alumno
 
 # Create your views here.
+
+def inicio(request):
+    alumnos = Alumno.objects.all()
+    return render(request, 'Alumno/inicio.html', {'alumnos': alumnos})  
